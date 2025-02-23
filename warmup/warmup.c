4b6b93dfaa0530a4aa5c0478c89bec52199e2ee5
Thiago Nascimento <nascimenthiago@gmail.com>
2025-02-23 07:10:45 -0300

    Fix portable use of dirent

--- warmup/warmup.c
+++ warmup/warmup.c
@@ -6,6 +6,7 @@
 
 const char WARMUP_INSTANCE[] = "01-tecla-king/";
 const char INPUT_DIR[] = "input/";
+const char FILE_PREFIX[] = "instance_";
 
 int main() {
 
@@ -23,9 +24,14 @@ int main() {
 
     if (dir) {
 
+        int prefix_length = strlen(&FILE_PREFIX);
         total_success = total_failures = 0;
 
-        while ((str_dir  = readdir(dir)) != NULL && str_dir->d_type == DT_REG) {
+        while ((str_dir  = readdir(dir)) != NULL) {
+
+            if (strncmp(str_dir->d_name, FILE_PREFIX, prefix_length)) {
+                continue;
+            }
 
             file_path[0] = '\0';
             strcat(file_path, WARMUP_INSTANCE);
@@ -40,7 +46,7 @@ int main() {
 
             solve_warmup(fptr, str_dir->d_name, WARMUP_INSTANCE);
             success = check_warmup_solution(str_dir->d_name, WARMUP_INSTANCE);
-            
+
             printf("Test %s %s.\n", str_dir->d_name, success ? "SUCCESS" : "FAILURE");
 
             if (success) {
@@ -48,7 +54,7 @@ int main() {
             } else {
                 total_failures++;
             }
-        
+
             fclose(fptr);
         }
 
